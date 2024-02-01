d71b009a3cbe33956473262c85fe7ed382044d9a
Oleg <mail@mail.com>
2024-02-01 21:07:56 +0300

    Complete playbook init-openvpn

--- scripts/generate-ssh-keys/generate-ssh.sh
+++ scripts/generate-ssh-keys/generate-ssh.sh
@@ -1,3 +1,7 @@
 # Run manually
 ssh-keygen -t rsa -b 4096 -f id_rsa
 ssh-copy-id -i ./id_rsa user@host
+
+# Выполнить на целевой машине. Позволяет не спрашивать пароль при вызове sudo root. Полезно для ansible
+echo "user ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers.d/user
+# todo - попробовать модифицировать, чтобы была возможность это делать не на целевой машине
